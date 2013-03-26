7d54b01776d0673c4a3381fb0c660344912b0251
zhangqiuming <zhqm03@gmail.com>
2013-03-26 18:53:23 +0800

    add system install tools

--- bash/install.sh
+++ bash/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+alias apti='apti'
 install_ruby(){
     curl -L https://get.rvm.io | bash -s stable --autolibs=3 --ruby
     gem install rails
@@ -7,37 +7,56 @@ install_ruby(){
 }
 
 install_python(){
-    apt-get install python python-pip python-virtualenv
+    apti python python-pip python-virtualenv
     pip install ipython
     pip install django<5.0
 }
 
 install_php(){
-    apt-get install php5 php5-cgi php5-cli php5-fpm php5-pear 
+    apti php5 php5-cgi php5-cli php5-fpm php5-pear php5-curl php5-dev php5-mysql
     curl -s http://getcomposer.org/installer | php
 }
 
 install_git(){
-    apt-get install git git-flow git-svn
+    apti git git-flow git-svn
 }
 
 install_dev(){
-    apt-get install build_essential
+    apti build_essential
     install_php
     install_python
     insta_ruby
 }
 
 install_db(){
-    apt-get install sqlite3 
-    apt-get install mysql-cli mysql-server mysql-common
-    apt-get install memcache
-    apt-get install redis
+    apti sqlite3 
+    apti mysql-cli mysql-server mysql-common
+    apti memcache
+    apti redis-server
 }
 
 install_nginx(){
-    apt-get install nginx
+    apti nginx
     nginx
+}
+
+install_operate(){
+    apti tree, lsof
+    apti cscope, exuberant-ctags
+    apti wget, curl, openssl, w3m
+    apti expect, lrzsz, tcl8.5 tcl8.5-dev
+    apti strace
+    apti zsh
+}
+
+install_front(){
+    apti coffeescript
+    apti nodejs, npm
+}
+
+install_cpp(){
+    apti gcc
+    apti libboost
 }
 
 usage(){
