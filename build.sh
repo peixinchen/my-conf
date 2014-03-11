a0cc2e51c11a3e50db3db593548bdd0bf7840859
reedboat <zhqm03@gmail.com>
2014-03-11 23:05:25 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+basedir=`pwd`
 
 function build_vim(){
-  ln -s ~/workspace/github/my-conf/vim/.vim ~/.vim
-  ln -s ~/workspace/github/my-conf/vim/.vimrc ~/.vimrc
+  ln -s $basedir/vimfiles/vim ~/.vim
+  ln -s $basedir/vimfiles/vimrc ~/.vimrc
 
   mkdir -p ~/.vim/bundle
   git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
   vim +BundleInstall +qa
-  git submodule init; git submodule update
+  git submodule init; 
+  git submodule update
 
   if [[ `which rvm` ]]
   then
+      if [[ -d "~/.vim/bundle/Command-T" ]]
        cd ~/.vim/bundle/Command-T/ruby/command-t/
        rvm system do ruby extconf.rb
        make
@@ -20,19 +23,23 @@ function build_vim(){
 
 function build_bash(){
    echo 'build bash' 
+   ln -s $basedir/bash/bash_profile ~/.bash_profile
+   ln -s $basedir/bash/bash_alias ~/.bash_alias
+   ln -s $basedir/bash/markrc ~/.markrc
+   # install zsh
+   cd ~
+   git clone https://github.com/robbyrussell/oh-my-zsh.git 
+   ln -s $basedir/bash/zshrc ~/.zshrc
+   ln -s $basedir/screen/screenrc ~/.screenrc
+   mkdir ~/bin
+   cp $basedir/ssh.exp ~/bin/
+   cp $basedir/*.sh ~/bin/
+   chown +x ~/bin/*.sh ~/bin/*.exp
 }
 
-function usage(){
-    echo 'usage'
-}
-
-function build_default(){
-    echo 'build_default'
-}
 
 case $1 in
     vim) build_vim;;
-    screen) build_screen;;
     bash) build_bash;;
     *) usage
 esac
